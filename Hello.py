e68e233b607633491f9458f56bf17068ce888925
Grant <grantshanklin@gmail.com>
2020-05-22 22:45:59 -0500

    Added labels

---
+++ Hello.py
@@ -0,0 +1,12 @@
+﻿from tkinter import *
+
+root = Tk()
+
+#Creates a label widget
+myLabel1 = Label(root, text="Hello World!")
+myLabel2 = Label(root, text="My name is Grant Shanklin")
+#Puts it onto the screen
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=1)
+
+root.mainloop()
